e74ecf8574d3821468ea41af7474682ce81516df
Kevin Mahoney <kevin.mahoney@everops.com>
2020-06-04 00:31:55 -0400

    fix

--- outputs.tf
+++ outputs.tf
@@ -14,17 +14,17 @@ output "dns_name" {
 }
 
 output "security_group_id" {
-  value       = aws_security_group.this.id
+  value       = aws_security_group.this[*].id
   description = "EFS Security Group ID"
 }
 
 output "security_group_arn" {
-  value       = aws_security_group.this.arn
+  value       = aws_security_group.this[*].arn
   description = "EFS Security Group ARN"
 }
 
 output "security_group_name" {
-  value       = aws_security_group.this.name
+  value       = aws_security_group.this[*].name
   description = "EFS Security Group name"
 }
 
